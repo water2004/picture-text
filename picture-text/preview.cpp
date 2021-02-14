61aea961156c6fd74b4770db143ada00099ae5e4
GBwater <3076697508@qq.com>
2021-02-14 23:29:27 +0800

    更新未保存文件里的注释

--- picture-text/preview.cpp
+++ picture-text/preview.cpp
@@ -13,17 +13,17 @@ preview::~preview()
     delete ui;
 }
 
-void preview::setPic(QPixmap *pic)
+void preview::setPic(QPixmap *pic)//设置图片
 {
     ui->label->sourse=pic;
 }
 
-void preview::init()
+void preview::init()//初始化
 {
     ui->label->init();
 }
 
 void preview::refresh()
 {
-    ui->label->refresh(0,0);
+    ui->label->refresh(0,0);//mx,my设为0,移动的距离也是0,只更新图片
 }
